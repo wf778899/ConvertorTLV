e655995c1989d80547e570a59e2ea2d528ca3d32
Master <wf778899@bk.ru>
2023-03-04 11:32:37 +0300

    Added platform detection in CMake. Global setting of MSVC specific flags

--- Tests/Test_TLV.cpp
+++ Tests/Test_TLV.cpp
@@ -1,2 +1,18 @@
 #include "TLVObject.h"
+
 #include <gtest/gtest.h>
+
+
+TEST(TLVObjectTests, Test_WriteBool)
+{
+    TLVObject tlv;
+    auto expected = true;
+    ASSERT_EQ( tlv.WriteBool(true), expected );
+}
+
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
